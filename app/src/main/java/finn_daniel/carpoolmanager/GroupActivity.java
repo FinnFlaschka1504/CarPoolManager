fb6347c96b7a61f5319cac231aa40da5f63ad159
Finn Flaschka <finn.flaschka@gmail.com>
2019-07-10 15:04:36 +0200

    Trip Listen hinzugefügt

--- app/src/main/java/finn_daniel/carpoolmanager/GroupActivity.java
+++ app/src/main/java/finn_daniel/carpoolmanager/GroupActivity.java
@@ -1,16 +1,19 @@
 package finn_daniel.carpoolmanager;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -27,6 +30,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -158,12 +163,14 @@ class ViewPager_GroupOverview extends Fragment {
     Gson gson = new Gson();
     String EXTRA_GROUP = "EXTRA_GROUP";
     String EXTRA_PASSENGERMAP = "EXTRA_PASSENGERMAP";
+    Dialog dialog_tripList;
 
 
     Button overview_showAllTrips;
     Button overview_showMyTrips;
     TextView overview_groupName;
     Switch overview_isDriverSwitch;
+    ListView dialogTripList_list;
 
 
     List<String> listviewTitle = new ArrayList<String>();
@@ -213,12 +220,144 @@ class ViewPager_GroupOverview extends Fragment {
             }
         });
 
+        overview_showAllTrips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!overview_showAllTrips.getText().toString().equals("0"))
+                    showTripList(true);
+                else
+                    Toast.makeText(getContext(), "Es gibt nix zum anzeigen", Toast.LENGTH_SHORT).show();
+            }
+        });
+        overview_showMyTrips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!overview_showMyTrips.getText().toString().equals("0"))
+                    showTripList(false);
+                else
+                    Toast.makeText(getContext(), "Es gibt nix zum anzeigen", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         overview_isDriverSwitch.setChecked(thisGroup.getDriverIdList().contains(loggedInUser.getUser_id()));
         reLoadContent();
 
         return view;
 
         // ToDo: Lade Daten aus der Cloud und passe an bei Änderungen
+    }
+
+    private void showTripList(boolean showAll) {
+        dialog_tripList = new Dialog(getContext());
+        dialog_tripList.setContentView(R.layout.dialog_trip_list);
+
+        dialogTripList_list = dialog_tripList.findViewById(R.id.dialogTripList_list);
+
+        dialog_tripList.findViewById(R.id.dialogTripList_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog_tripList.dismiss();
+            }
+        });
+
+        List<String> tripList_date_list =  new ArrayList<>();
+        List<String> tripList_driver_list =  new ArrayList<>();
+        List<String> tripList_fromTo_list =  new ArrayList<>();
+        List<String> tripList_distance_list =  new ArrayList<>();
+        List<String> tripList_twoWay_list =  new ArrayList<>();
+        List<String> tripList_cost_list =  new ArrayList<>();
+
+        List<Trip> tripList = new ArrayList<>();
+        for (String tripId : thisGroup.getTripIdList()) {
+            Trip trip = groupTripsMap.get(tripId);
+            if (!showAll && !trip.getDriverId().equals(loggedInUser.getUser_id()))
+                continue;
+            tripList.add(trip);
+        }
+
+        Collections.sort(tripList, new Comparator() {
+            public int compare(Trip obj1, Trip obj2) {
+                return obj1.getDate().compareTo(obj2.getDate());
+            }
+
+            public int compare(Object var1, Object var2) {
+                return this.compare((Trip) var1, (Trip) var2);
+            }
+        });
+
+        String pattern = "dd.MM.yyyy E";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        for (Trip trip: tripList) {
+            tripList_date_list.add(simpleDateFormat.format(trip.getDate()).replace(" ", " (") + ")");
+            tripList_driver_list.add(showAll ?
+                    "- " + groupPassengerMap.get(trip.getDriverId()).getUserName() : "");
+            tripList_fromTo_list.add(trip.getLocationName().get(0) +
+                    (trip.isTwoWay() ? " ⇔ " : " ⇒ ") + trip.getLocationName().get(1));
+            tripList_distance_list.add(trip.getDistance());
+            tripList_twoWay_list.add(trip.isTwoWay() ? "x2" : "");
+            tripList_cost_list.add(df.format(trip.getCost()) + "€");
+        }
+
+        ArrayList<HashMap<String, Serializable>> aList = new ArrayList<HashMap<String, Serializable>>();
+
+        for(int i = 0; i < tripList.size(); ++i) {
+            HashMap<String, Serializable> hm = new HashMap<String, Serializable>();
+            (hm).put("tripList_date_list", tripList_date_list.get(i));
+            (hm).put("tripList_driver_list", tripList_driver_list.get(i));
+            (hm).put("tripList_fromTo_list", tripList_fromTo_list.get(i));
+            (hm).put("tripList_distance_list", tripList_distance_list.get(i));
+            (hm).put("tripList_twoWay_list", tripList_twoWay_list.get(i));
+            (hm).put("tripList_cost_list", tripList_cost_list.get(i));
+            aList.add(hm);
+        }
+
+        String[] from = new String[]{"tripList_date_list", "tripList_driver_list", "tripList_fromTo_list", "tripList_distance_list", "tripList_twoWay_list", "tripList_cost_list"};
+        int[] to = new int[]{R.id.tripList_date, R.id.tripList_driver, R.id.tripList_fromTo, R.id.tripList_distance, R.id.tripList_twoWay, R.id.tripList_cost};
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this.getContext(), aList, R.layout.trip_list_item, from, to);
+        dialogTripList_list.setAdapter(simpleAdapter);
+
+        // ToDo: fahrten nach anteil farblich markieren
+
+//      <undefinedtype> mOnPreDrawListener = new OnPreDrawListener() {
+//            public boolean onPreDraw() {
+//                ListView var10000 = (ListView)ReminderListe.this._$_findCachedViewById(id.listView_reminder);
+//                Intrinsics.checkExpressionValueIsNotNull(var10000, "listView_reminder");
+//                ListAdapter listAdapter = var10000.getAdapter();
+//                int i = 0;
+//                Intrinsics.checkExpressionValueIsNotNull(listAdapter, "listAdapter");
+//
+//                for(int var3 = listAdapter.getCount(); i < var3; ++i) {
+//                    ReminderListe var6 = ReminderListe.this;
+//                    ListView var10002 = (ListView)ReminderListe.this._$_findCachedViewById(id.listView_reminder);
+//                    Intrinsics.checkExpressionValueIsNotNull(var10002, "listView_reminder");
+//                    View listItem = var6.getViewByPosition(i, var10002);
+//                    View var7 = listItem.findViewById(-1000011);
+//                    if (var7 == null) {
+//                        throw new TypeCastException("null cannot be cast to non-null type android.widget.TextView");
+//                    }
+//
+//                    TextView status_description_view = (TextView)var7;
+//                    if (Intrinsics.areEqual(status_description_view.getText(), ReminderListe.this.getDEAKTIVIERT$app_debug())) {
+//                        status_description_view.setTextColor(-65536);
+//                    }
+//                }
+//
+//                return true;
+//            }
+//        };
+
+
+
+
+
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog_tripList.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        dialog_tripList.show();
+        dialog_tripList.getWindow().setAttributes(lp);
     }
 
     public void setData(User pLoggedInUser, Group pThisGroup, Map<String,User> pGroupPassengerMap, Map<String,Trip> pGroupTripsMap) {
